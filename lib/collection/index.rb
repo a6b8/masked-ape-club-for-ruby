5cfa381533c9e15a698feb9a56f7f6b05ab59cb1
a6b8 <andreasbanholzer@me.com>
2021-12-28 09:21:56 +0100

    hello world

--- lib/collection/index.rb
+++ lib/collection/index.rb
@@ -2,11 +2,10 @@ require 'json'
 
 module Collection
     def self.load( gateway )
-        path = '../lib/collection/0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D.json'
+        path = './lib/collection/0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D.json'
         data = open( path ).read()
         result = JSON.parse( data )
 
-        # puts "result: #{JSON.parse( result['data'][0]['metadata'] )['image']}"
         result = result['data'].map { | a | 
             a['metadata'] = JSON.parse( a['metadata'] )
             a['metadata']['image'] = a['metadata']['image']
@@ -14,9 +13,7 @@ module Collection
                 .last.insert( 0, gateway )
             a 
         }
-        
-        # puts "url: #{result[0]['metadata']['image']}"
-    
+
         return result
     end
 end
