9dea624161eca529b80c29c592cfb0bb6deb8832
a6b8 <andreasbanholzer@me.com>
2021-12-28 22:36:37 +0100

    hello world

--- test/test-outside.rb
+++ test/test-outside.rb
@@ -5,13 +5,13 @@ a = MaskedApeClub::Load.new()
 item = {
     id: 1711,
     fuzz: '1%',
-    path: './test/result/test.png',
+    path: 'result/test.png',
     background: {
-        path: './test/template/ice.png',
+        path: 'template/ice.png',
         size: 400
     }
 }
 
 a.load( id: item[:id] )
 a.mask_ape( id: item[:id], fuzz: item[:fuzz] )
-a.write( path: './test/preview/mask_ape.png' )
+a.write( path: 'preview/mask_ape.png' )
